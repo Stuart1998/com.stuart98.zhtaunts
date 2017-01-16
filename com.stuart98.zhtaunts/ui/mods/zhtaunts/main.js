616bdb6917c4920b4501b212768a3b38e9cc41ac
Stuart Hepworth <robostuart@gmail.com>
2017-01-16 10:34:27 -0700

    Easy Stuff

---
+++ com.stuart98.zhtaunts/ui/mods/zhtaunts/main.js
@@ -0,0 +1,151 @@
+$(document).ready(function () {
+    flubb_sfx.addHandler('01', function () {
+              new Audio('coui://ui/mods/zhtaunts/gd.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('02', function () {
+              new Audio('coui://ui/mods/zhtaunts/ambulance.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('03', function () {
+              new Audio('coui://ui/mods/zhtaunts/dontdt.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('04', function () {
+              new Audio('coui://ui/mods/zhtaunts/goml.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('05', function () {
+              new Audio('coui://ui/mods/zhtaunts/gomp.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('06', function () {
+              new Audio('coui://ui/mods/zhtaunts/really.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('07', function () {
+              new Audio('coui://ui/mods/zhtaunts/fredsneak.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('08', function () {
+              new Audio('coui://ui/mods/zhtaunts/sneaky.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('09', function () {
+              new Audio('coui://ui/mods/zhtaunts/hurt.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('10', function () {
+              new Audio('coui://ui/mods/zhtaunts/maskhurt.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('11', function () {
+              new Audio('coui://ui/mods/zhtaunts/power.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('12', function () {
+              new Audio('coui://ui/mods/zhtaunts/putp.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('13', function () {
+              new Audio('coui://ui/mods/zhtaunts/trap.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('14', function () {
+              new Audio('coui://ui/mods/zhtaunts/expand.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('15', function () {
+              new Audio('coui://ui/mods/zhtaunts/expandfast.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('16', function () {
+              new Audio('coui://ui/mods/zhtaunts/wgt.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('17', function () {
+              new Audio('coui://ui/mods/zhtaunts/wwt.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('18', function () {
+              new Audio('coui://ui/mods/zhtaunts/ew.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('19', function () {
+              new Audio('coui://ui/mods/zhtaunts/putp2.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('20', function () {
+              new Audio('coui://ui/mods/zhtaunts/badteam1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('21', function () {
+              new Audio('coui://ui/mods/zhtaunts/ego1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('22', function () {
+              new Audio('coui://ui/mods/zhtaunts/feelbad1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('23', function () {
+              new Audio('coui://ui/mods/zhtaunts/gg1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('24', function () {
+              new Audio('coui://ui/mods/zhtaunts/holkins1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('25', function () {
+              new Audio('coui://ui/mods/zhtaunts/lostgame.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('26', function () {
+              new Audio('coui://ui/mods/zhtaunts/lowenergy.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('27', function () {
+              new Audio('coui://ui/mods/zhtaunts/mistake1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('28', function () {
+              new Audio('coui://ui/mods/zhtaunts/moon1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('29', function () {
+              new Audio('coui://ui/mods/zhtaunts/moon2.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('30', function () {
+              new Audio('coui://ui/mods/zhtaunts/run1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('31', function () {
+              new Audio('coui://ui/mods/zhtaunts/run2.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('32', function () {
+              new Audio('coui://ui/mods/zhtaunts/screwed1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('33', function () {
+              new Audio('coui://ui/mods/zhtaunts/screwed2.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('34', function () {
+              new Audio('coui://ui/mods/zhtaunts/upset1.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('35', function () {
+              new Audio('coui://ui/mods/zhtaunts/upset2.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('36', function () {
+              new Audio('coui://ui/mods/zhtaunts/upset3.ogg').play();
+           
+    }, true);
+    flubb_sfx.addHandler('37', function () {
+              new Audio('coui://ui/mods/zhtaunts/yoda1.ogg').play();
+           
+    }, true);
+ 
+});
